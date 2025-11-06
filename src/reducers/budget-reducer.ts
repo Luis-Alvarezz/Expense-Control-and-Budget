67b977273694ca8f6548e95ae3744c1807326553
Luis-Alvarezz <luisangelug10@gmail.com>
2025-11-05 21:20:16 -0600

    [CREATE]: useReducer y primer metodo para agregar presupuestos

---
+++ src/reducers/budget-reducer.ts
@@ -0,0 +1,27 @@
+// * Si fuera mas grande (mas secciones) seria buena idea dividirlo en varias secciones, pero aqui tendremos todo.
+
+// ! 1.- Métodos o Acciones para 
+export type BudgetActions = 
+ { type: 'add-budget', payload: {budget: number}}
+
+//  ! 2.- STATE
+export type BudgetState = {
+  budget: number
+}
+
+// ! 3.- STATE INICIAL para cada Presupuesto o Budget
+export const initialState: BudgetState = {
+  budget: 0
+}
+
+// ! 4.- Reducer:
+export const budgetReducer = ( state: BudgetState = initialState, action: BudgetActions  ) => {
+  if (action.type === 'add-budget') {
+    return {
+      ...state,
+      budget: action.payload.budget // * Colocamos directamente porque ya validamos en su Componente
+    }
+  }
+
+  return state;
+}
