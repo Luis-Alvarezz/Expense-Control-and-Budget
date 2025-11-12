fc5626807d22dd7abf243dd682c63a23da8e4496
Luis-Alvarezz <luisangelug10@gmail.com>
2025-11-11 21:26:28 -0600

    [ADDED]: Se añadio ContextAPI para envio de valores en Formulario y llenar el STATE

--- src/components/BudgetForm.tsx
+++ src/components/BudgetForm.tsx
@@ -1,8 +1,10 @@
 import { useMemo, useState } from "react"
+import { useBudget } from "../hooks/useBudget"
 
 export default function BudgetForm() {
   // * Requerimos estado inicial para validar formulario y si pasa la validacion, escribimos en nuestro useReducer:
   const [budget, setBudget] = useState(0)
+  const { dispatch } = useBudget() // * useReducer retornaba [state, dispatch], pero el customHook retorna {state, dispatch}
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     // console.log(e.target.name)
@@ -17,9 +19,18 @@ export default function BudgetForm() {
     // console.log(isNaN(budget)) // * 20, 40 -> False, porque SI es numero | '' -> TRUE porque NO es numero
     return isNaN(budget) || budget <= 0
   }, [budget]) // * Cada que budget cambie, queremos revisar esta funcion
+
+  // ! Método 3.- Envio de Formulario 
+  const handleSubmit = (e : React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    // console.log('Añadir o Definir Presupuesto')
+
+    // TODO - Usamos el dispach
+    dispatch({type: 'add-budget', payload: {budget}})
+  }
   
   return (
-    <form action="" className="space-y-5">
+    <form action="" className="space-y-5" onSubmit={ handleSubmit }>
       <div className="flex flex-col space-y-5">
         <label htmlFor="budget" className="text-4xl text-blue-600 font-bold text-center">
           Definir Presupuesto
@@ -39,7 +50,7 @@ export default function BudgetForm() {
       <input
         type="submit"
         value='Definir Presupuesto'
-        className="bg-blue-600 hover:bg-blue-700 cursor-pointer w-full p-2 text-white font-black uppercase disabled:opacity-40 disabled:cursor-not-allowed"
+        className="bg-blue-600 hover:bg-blue-700 cursor-pointer w-full p-2 text-white font-black uppercase disabled:opacity-40 disabled:cursor-not-allowed disabled:hover:bg-blue-600"
         disabled={isValid}
         />
     </form>
