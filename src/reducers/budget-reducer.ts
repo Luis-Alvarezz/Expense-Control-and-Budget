a4b30af0b1bacb297fbf738e436b4b11e56e06ec
Luis-Alvarezz <luisangelug10@gmail.com>
2025-11-23 22:08:39 -0600

    [ADDED]: Eliminar Expense y modal para formulario y llenar gasto

--- src/reducers/budget-reducer.ts
+++ src/reducers/budget-reducer.ts
@@ -1,18 +1,36 @@
 // * Si fuera mas grande (mas secciones) seria buena idea dividirlo en varias secciones, pero aqui tendremos todo.
+import type { DraftExpense, Expense } from "../types"
+import { v4 as uuidv4 } from "uuid"
 
 // ! 1.- Métodos o Acciones para 
 export type BudgetActions = 
- { type: 'add-budget', payload: {budget: number}}
+ { type: 'add-budget', payload: {budget: number} } |
+ { type: 'show-modal' } |
+ { type: 'close-modal' } |
+ { type: 'add-expense', payload: { expense: DraftExpense} } | // * El ID se genera en el reducer
+ { type: 'delete-expense', payload: { id: Expense['id']} } |
+ { type: 'update-expense', payload: {id: Expense['id']}, expense: Expense }
 
 //  ! 2.- STATE
 export type BudgetState = {
   budget: number
+  modal: boolean
+  expenses: Expense[]
 }
 
 // ! 3.- STATE INICIAL para cada Presupuesto o Budget
 export const initialState: BudgetState = {
   budget: 0,
   // auth: true // * Ejemplo al mostrar el console.log del state del CustomHook
+  modal: false,
+  expenses: []
+}
+
+const createExpense = (draftExpense: DraftExpense) : Expense => {
+  return {
+    ...draftExpense,
+    id: uuidv4()
+  }
 }
 
 // ! 4.- Reducer:
@@ -21,6 +39,46 @@ export const budgetReducer = ( state: BudgetState = initialState, action: Budget
     return {
       ...state,
       budget: action.payload.budget // * Colocamos directamente porque ya validamos en su Componente
+    }
+  }
+
+  if (action.type === 'show-modal') {
+    return {
+      ...state,
+      modal: true
+    }
+  }
+
+  if (action.type === 'close-modal') {
+    return {
+    ...state,
+    modal: false  
+    }
+  }
+
+  if (action.type === 'add-expense') {
+    const expense = createExpense( action.payload.expense)
+    
+    return {
+      ...state,
+      expenses: [...state.expenses, expense],
+      modal: false // * CERRAR MODAL
+    }
+  }
+
+  if (action.type === 'delete-expense') {
+    const items = state.expenses.filter( expense => expense.id !== action.payload.id)
+
+    return {
+      ...state,
+      expenses: items
+    }
+  }
+
+  if (action.type === 'update-expense') {
+    console.log('Actualizando...')
+    return {
+      ...state
     }
   }
 
