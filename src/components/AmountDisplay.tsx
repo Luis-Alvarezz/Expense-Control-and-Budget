ccff55f55db1cce1fcf7de9e4a7e64b4521322ee
Luis-Alvarezz <luisangelug10@gmail.com>
2025-11-25 20:26:07 -0600

    [MODIFY]: Evaluacion Logica AND para mostrar ':' si hay Label o nada si esta vacio

--- src/components/AmountDisplay.tsx
+++ src/components/AmountDisplay.tsx
@@ -1,15 +1,16 @@
 import { formatCurrency } from "../helpers/helper"
 
 type AmountDisplayProps = {
-  label: string,
-  amount: number
+  label?: string, // ! Con '?' Indicamos que es valor OPCIONAL !!!!
+  amount: number | string
 }
 
 export default function AmountDisplay({label, amount} : AmountDisplayProps) {
   return (
     <p className="text-2xl text-blue-600 font-bold">
-      {label}: {''}
-      <span className="font-black text-black">{formatCurrency ( amount )}</span>
+      {/* {label}: {''} */}
+      { label && `${label}: `}
+      <span className="font-black text-black">{formatCurrency ( Number(amount) )}</span>
     </p>
   )
 }
