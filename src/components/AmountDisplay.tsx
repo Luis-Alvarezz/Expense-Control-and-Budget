5450da81a974589bec234ed57c58799a93b8e4bb
Luis-Alvarezz <luisangelug10@gmail.com>
2025-11-25 20:26:14 -0600

    [MODIFY]: Evaluacion Logica AND para mostrar ':' si hay Label o nada si esta vacio

--- src/components/AmountDisplay.tsx
+++ src/components/AmountDisplay.tsx
@@ -14,3 +14,14 @@ export default function AmountDisplay({label, amount} : AmountDisplayProps) {
     </p>
   )
 }
+/*
+  evaluación lógica AND:
+  Si label tiene un valor truthy (texto, string no vacío), entonces React renderiza lo que está después del &&.
+  Si label es undefined, null, '' (string vacío), 0, false, entonces NO renderiza nada.
+
+  label = "Presupuesto"  --> muestra "Presupuesto: "
+  label = "Disponible"   --> muestra "Disponible: "
+  label = "" (string vacío)  --> NO muestra nada
+  label = undefined  --> NO muestra nada
+
+*/
