cb8935537ad9d3ee1f57700a3ed3a0e10672d10f
Luis-Alvarezz <luisangelug10@gmail.com>
2025-11-27 20:28:34 -0600

    [MODIFY]: Se añadio el calculo de gasto Usado y Disponible de manera Global

--- src/context/BudgetContext.tsx
+++ src/context/BudgetContext.tsx
@@ -1,4 +1,4 @@
-import { useReducer, createContext } from "react"
+import { useReducer, createContext, useMemo } from "react"
 import { budgetReducer, initialState, type BudgetActions, type BudgetState } from "../reducers/budget-reducer"
 
 // ! 4. Type del PROVIDER para Children
@@ -11,6 +11,8 @@ type BudgetContextProps = {
   // * Aqui se conecta el CONTEXT (la accion de tener el estado global) y PROVIDER (los datos que va a tener el CONTEXT)
   state: BudgetState
   dispatch: React.Dispatch<BudgetActions> // * GENERIC (<>) para indicar que es dinamico por las acciones
+  totalExpenses: number
+  reminingBudget: number
 }
 
 // ! 2.- Crear el Context (La accion de tener el STATE GLOBAL)
@@ -22,11 +24,17 @@ export const BudgetContext = createContext<BudgetContextProps>(null!) // * Opcio
 export const BudgetProvider = ({children}: BudgetProviderProps) => {
   const [state, dispatch] = useReducer(budgetReducer, initialState)
   
+  // ! Calcular el gasto usado y Disponible:
+  const totalExpenses = useMemo(() => state.expenses.reduce((total, expense) => Number(expense.amount) + total, 0) , [state.expenses])
+  const reminingBudget = useMemo(() => state.budget - totalExpenses, [state.budget, totalExpenses])
+  
   return (
     <BudgetContext.Provider
       value={{ // * El value siempre es OBJETO, en esta caso retornamos otro OBJETO
         state,
-        dispatch
+        dispatch,
+        totalExpenses,
+        reminingBudget
       }}
     >
       {children}
