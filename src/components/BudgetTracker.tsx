e47625c243910452e87d5afa1bbe5a0c5631404d
Luis-Alvarezz <luisangelug10@gmail.com>
2025-11-27 21:50:58 -0600

    [MODIFY]: Grafica de Gastos Dinamica

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -1,14 +1,29 @@
 import AmountDisplay from "./AmountDisplay";
 import { useBudget } from "../hooks/useBudget";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css"
 
 
 export default function BudgetTracker() {
   const { state, totalExpenses, reminingBudget } = useBudget()
 
+  const percentage = +((totalExpenses / state.budget) * 100).toFixed(2) // * + Para convertir de STRING a NUMERO |  MAX 2 Decimales
+  console.log(percentage)
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
       <div className="flex justify-center">
-        <img src="/grafico.jpg" alt="Gráficos de gastos" />
+        {/* <img src="/grafico.jpg" alt="Gráficos de gastos" /> */}
+        <CircularProgressbar 
+          value={percentage}
+          styles={buildStyles({
+            pathColor: percentage === 100 ? '#DC2626' : '#3B82F6', // * Color a lo que se ha utilizado
+            trailColor: '#F5F5F5', // * Color que NO se ha utilizado
+            textSize: 8,
+            textColor: percentage === 100 ? '#DC2626' : '#3B82F6',
+          })}
+          text={`${percentage}% Gastado`}
+        />
 
       </div>
 
